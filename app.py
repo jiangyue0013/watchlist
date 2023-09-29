b2ba400013d24cd20cbd2bf046bf04aa923d8556
Jiang Yue <jiangyue0013@foxmail.com>
2023-09-29 18:21:02 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -29,10 +29,19 @@ class Movie(db.Model):
 
 
 @app.route("/")
-def hello_world():
-    user = User.query.first()
+def index():
     movies = Movie.query.all()
-    return render_template("index.html", user=user, movies=movies)
+    return render_template("index.html", movies=movies)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
 
 @app.cli.command()
